39abe72344fb1d3e066852627afb3c682c6400dc
Kovalenko Vasyl <kovalenkova26@gmail.com>
2022-07-10 13:42:42 +0300

    add user

---
+++ src/users/users.controller.ts
@@ -0,0 +1,32 @@
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
+import {CreateUserDto} from "./dto/create-user.dto";
+import {UsersService} from "./users.service";
+import {Roles} from "../auth/roles-auth.decorator";
+import {RolesGuard} from "../auth/roles.guard";
+import {AddRoleDto} from "./dto/add-role.dto";
+import {BanUserDto} from "./dto/ban-user.dto";
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private usersService: UsersService) {}
+
+    @Post()
+    create(@Body() userDto: CreateUserDto) {
+        return this.usersService.createUser(userDto);
+    }
+
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Get("/role")
+    addRole(@Body() dto: AddRoleDto) {
+        return this.usersService.addRole(dto);
+    }
+
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Get("/ban")
+    ban(@Body() dto: BanUserDto) {
+        return this.usersService.ban(dto);
+    }
+}
---
+++ src/users/users.model.ts
@@ -0,0 +1,30 @@
+import {Table, Model, Column, DataType, BelongsToMany} from "sequelize-typescript";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles-model";
+
+interface UserCreationAttributes {
+    email: string;
+    password: string;
+}
+
+
+@Table({tableName: "users"})
+export class User extends Model<User, UserCreationAttributes>{
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @Column({type: DataType.STRING,  unique: true, allowNull: false})
+    email: string;
+
+    @Column({type: DataType.STRING, unique: true,  allowNull: false})
+    password: string;
+
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+
+    @Column({type: DataType.STRING, allowNull: true})
+    banReason: string;
+
+    @BelongsToMany(() => Role, () => UserRoles)
+    roles: Role[];
+}
