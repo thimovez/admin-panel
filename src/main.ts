9556311b8e6bb350e27c7a02dfe612708d6ab019
Kovalenko Vasyl <kovalenkova26@gmail.com>
2022-07-12 15:34:00 +0300

    add global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
+import {ValidationPipe} from "./pipes/validation.pipe";
 
 
 async function start(): Promise<void> {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule)
+
+
+    app.useGlobalPipes(new ValidationPipe())
+
     await app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
 }
 
